86179ad30932d90a8ac68883f10fbafd8552a57b
Muhammad Zubaidi <muhammadzubaidi333@gmail.com>
2022-10-13 12:30:02 +0700

    Clear

--- app/src/main/java/com/codepelita/myapplication/MainActivity.kt
+++ app/src/main/java/com/codepelita/myapplication/MainActivity.kt
@@ -3,7 +3,10 @@ package com.codepelita.myapplication
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.codepelita.myapplication.databinding.ActivityMainBinding
+import org.json.JSONException
+import org.json.JSONObject
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     var firstName: ArrayList<String> = ArrayList()
     var lastName: ArrayList<String> = ArrayList()
     var gender: ArrayList<String> = ArrayList()
+    var age: ArrayList<String> = ArrayList()
+    var number: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +25,30 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val linearLayoutManager = LinearLayoutManager(applicationContext)
+        binding.listData.layoutManager = linearLayoutManager
+        try {
+            var obj = JSONObject(loadJSon())
+            var jsonArray = obj.getJSONArray("people")
+            for (i in 0 until jsonArray.length()) {
+                val userDetail = jsonArray.getJSONObject(i)
+                firstName.add("Nama Depan : ${userDetail.getString("firstName")}")
+                lastName.add("Nama Belakang : ${userDetail.getString("lastName")}")
+                gender.add("Jenis Kelamin : ${userDetail.getString("gender")}")
+                age.add("Usia : ${userDetail.getString("age")}")
+                number.add("Nomor ID : ${userDetail.getString("number")}")
+            }
+        } catch (ex : JSONException) {
+            ex.printStackTrace()
+        }
+        val peopleAdapter = PeopleAdapter(this@MainActivity, firstName, lastName, gender, age, number)
+        binding.listData.adapter = peopleAdapter
     }
 
-    private fun loadJSon(context: Context): String? {
+    private fun loadJSon(): String? {
         val json: String?
         try {
-            val inputStream = context.assets.open("data_people.json")
+            val inputStream = assets.open("data_people.json")
             val size = inputStream.available()
             val buffer = ByteArray(size)
             inputStream.read(buffer)
--- app/src/main/java/com/codepelita/myapplication/PeopleAdapter.kt
+++ app/src/main/java/com/codepelita/myapplication/PeopleAdapter.kt
@@ -1,8 +1,10 @@
 package com.codepelita.myapplication
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class PeopleAdapter(
@@ -10,20 +12,31 @@ class PeopleAdapter(
     private var firstName: ArrayList<String>,
     private var lastName: ArrayList<String>,
     private var gender: ArrayList<String>,
-    ) : RecyclerView.Adapter<PeopleAdapter.ViewHolder>() {
+    private var age: ArrayList<String>,
+    private var number: ArrayList<String>,
+) : RecyclerView.Adapter<PeopleAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
+        var nama_depan = itemView.findViewById<TextView>(R.id.tvFirstName)
+        var nama_belakang = itemView.findViewById<TextView>(R.id.tvLastName)
+        var jkelamin = itemView.findViewById<TextView>(R.id.tvGender)
+        var usia = itemView.findViewById<TextView>(R.id.tvAge)
+        var nomor = itemView.findViewById<TextView>(R.id.tvNumber)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(context).inflate(R.layout.list_adapter, parent,false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.nama_depan.text = firstName[position]
+        holder.nama_belakang.text = lastName[position]
+        holder.jkelamin.text = gender[position]
+        holder.usia.text = age[position]
+        holder.nomor.text = number[position]
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return firstName.size
     }
 }
