958ced9384ac23ed3f64056bbc51a2d46c8ed812
Muhammad Zubaidi <muhammadzubaidi333@gmail.com>
2023-10-13 08:18:47 +0700

    tambah halaman 2

---
+++ app/src/main/java/com/codepelita/myapplication/halaman2.kt
@@ -0,0 +1,11 @@
+package com.codepelita.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class halaman2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman2)
+    }
+}
